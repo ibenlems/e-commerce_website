c2730cf49ff78889fe48b04f1a851eeb04908b2a
Youssra20 <57915166+Youssra20@users.noreply.github.com>
2020-04-23 14:41:07 +0200

    Add files via upload

---
+++ Projet/src/pack/Compte.java
@@ -0,0 +1,63 @@
+package pack;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Compte {
+
+	private String username;
+	private String password;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+
+	
+	@OneToOne
+	Vendeur ownerV;
+	Acheteur ownerA;
+	
+	public Compte(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
