7a96b839b26b013b50268fec41d7dca446707b02
chaimaabenabbou <benabbouchaimaa@gmail.com>
2020-04-25 15:19:43 +0200

    modif acheteur

--- Projet/src/pack/Acheteur.java
+++ Projet/src/pack/Acheteur.java
@@ -7,6 +7,10 @@ import javax.persistence.*;
 public class Acheteur implements Client {
 
 	Integer note;
+	String nom;
+	String prenom;
+	Compte compte;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	int id;
@@ -23,6 +27,30 @@ public class Acheteur implements Client {
 	}
 
 	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Compte getCompte() {
+		return compte;
+	}
+
+	public void setCompte(Compte compte) {
+		this.compte = compte;
+	}
+
 	public int getId() {
 		return id;
 	}
